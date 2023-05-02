97e023dba392d733da60113e18ab3d371ca6aa67
NullVibes <71892534+NullVibes@users.noreply.github.com>
2023-05-02 13:27:43 -0400

    Update osm-server.sh

--- osm-server.sh
+++ osm-server.sh
@@ -12,16 +12,17 @@ rpi_install() {
   #echo 'Disabling BT requires rebooting.'
   #sed '/\[all\]/a\ dtoverlay=disable-bt' /boot/config.txt
   
-  usermod -aG sudo renderaccount
-  
   apt install postgresql-13-postgis-3 postgresql-13-postgis-3-scripts -y
 }
 
 
 ubuntu_install () {
-  usermod -aG sudo renderaccount
-  
   apt install postgresql-12-postgis-3 postgresql-12-postgis-3-scripts -y
+  
+  #swapoff -a
+  #dd if=/dev/zero of=/swapfile bs=1M count=12288
+  #mkswap /swapfile
+  #swapon /swapfile
 }
 
 apt update && apt upgrade -y
@@ -30,6 +31,35 @@ apt install libboost-all-dev git tar unzip wget bzip2 build-essential autoconf l
 
 if uname -a | grep -i raspberry; then rpi_install; fi
 if uname -a | grep -i ubuntu; then ubuntu_install; fi
+
+useradd -m -g user -G user,sudo -s /bin/bash renderaccount
+passwd renderaccount
+su - postgres -c 'createuser renderaccount; createdb -E UTF8 -O renderaccount gis'
+su - postgres -c 'psql -d gis -c "CREATE EXTENSION postgis"'
+su - postgres -c 'psql -d gis -c "CREATE EXTENSION hstore"'
+su - postgres -c 'psql -d gis -c "ALTER TABLE geometry_columns OWNER TO renderaccount"'
+su - postgres -c 'psql -d gis -c "ALTER TABLE spatial_ref_sys OWNER TO renderaccount"'
+
+apt install npm osm2pgsql autoconf apache2-dev libtool libxml2-dev libbz2-dev libgeos-dev libgeos++-dev libproj-dev gdal-bin libmapnik-dev mapnik-utils python3-mapnik python3-psycopg2 python3-yaml -y
+
+mkdir ~/src
+cd ~/src
+git clone https://github.com/SomeoneElseOSM/mod_tile.git
+cd mod_tile
+./autogen.sh
+./configure
+make
+make install
+make install-mod_tile
+ldconfig
+
+su - renderaccount -c 'mkdir ~/src; cd ~/src; git clone https://github.com/gravitystorm/openstreetmap-carto;'
+su - renderaccount -c 'cd ~/src; npm install -g carto'
+su - renderaccount -c 'carto ~/src/openstreetmap-carto/project.mml > ~/src/openstreetmap-carto/mapnik.xml'
+su - renderaccount -c 'mkdir ~/data; cd ~/data; wget https://download.geofabrik.de/north-america/us-latest.osm.pbf'
+su - renderaccount -c 'osm2pgsql -d gis --create --slim  -G --hstore --tag-transform-script ~/src/openstreetmap-carto/openstreetmap-carto.lua -C 8192 --number-processes 2 -S ~/src/openstreetmap-carto/openstreetmap-carto.style ~/data/us-latest.osm.pbf'
+su - renderaccount -c 'cd ~/src/openstreetmap-carto; psql -d gis -f indexes.sql; scripts/get-external-data.py; scripts/get-fonts.sh'
+
 
 # Check RAM and resize Swap
 # free
