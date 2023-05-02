fd33e5214a4631b7f3a5f4835cf3c914ef1cf5bc
NullVibes <71892534+NullVibes@users.noreply.github.com>
2023-05-02 11:37:29 -0400

    Update osm-server.sh

--- osm-server.sh
+++ osm-server.sh
@@ -6,14 +6,15 @@
 
 rpi_install() {
 
-echo 'options g_ether host_addr='$(dmesg | awk '/: HOST MAC/{print $NF}')' dev_addr='$(dmesg | awk '/: MAC/{print $NF}') | sudo tee /etc/modprobe.d/g_ether.conf
+  echo 'options g_ether host_addr='$(dmesg | awk '/: HOST MAC/{print $NF}')' dev_addr='$(dmesg | awk '/: MAC/{print $NF}') | sudo tee /etc/modprobe.d/g_ether.conf
 
-# Disable BT
-#echo 'Disabling BT requires rebooting.'
-#sed '/\[all\]/a\ dtoverlay=disable-bt' /boot/config.txt
+  # Disable BT
+  #echo 'Disabling BT requires rebooting.'
+  #sed '/\[all\]/a\ dtoverlay=disable-bt' /boot/config.txt
 
-apt update && apt upgrade -y
+  apt update && apt upgrade -y
 }
+
 
 ubuntu_install () {
 }
@@ -21,9 +22,6 @@ ubuntu_install () {
 
 if uname -a | grep -i raspberry; then rpi_install; fi
 if uname -a | grep -i ubuntu; then ubuntu_install; fi
-
-apt update && apt upgrade -y
-
 
 # Check RAM and resize Swap
 # free
