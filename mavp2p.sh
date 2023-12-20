8ed50facca212b67c614f4a8aedc8ee20c0c3094
NullVibes <71892534+NullVibes@users.noreply.github.com>
2023-12-20 15:33:29 -0500

    Update mavp2p.sh

--- mavp2p.sh
+++ mavp2p.sh
@@ -8,3 +8,9 @@ rm go1.21.5.linux-arm64.tar.gz
 echo 'PATH=$PATH:/usr/local/go/bin' | tee -a ~/.profile &>/dev/null
 echo 'GOPATH=$HOME/go' | tee -a ~/.profile &>/dev/null
 source ~/.profile
+
+cd ~/src
+wget https://github.com/bluenviron/mavp2p/releases/download/v1.2.0/mavp2p_v1.2.0_linux_arm64v8.tar.gz
+sudo tar -C /usr/local/bin -xzf mavp2p_v1.2.0_linux_arm64v8.tar.gz
+rm mavp2p_v1.2.0_linux_arm64v8.tar.gz
+
