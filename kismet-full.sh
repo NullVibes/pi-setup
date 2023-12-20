418ab9c4162fb621d887f7adbb75fce0cb7825b5
NullVibes <71892534+NullVibes@users.noreply.github.com>
2023-12-20 17:29:07 -0500

    Update kismet-full.sh

--- kismet-full.sh
+++ kismet-full.sh
@@ -10,7 +10,6 @@ if [[ $CONT == "n" ]]; then
   exit
 fi
 
-
 wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
 
 if [[ $DISTID == "Ubuntu" ]]; then
@@ -42,8 +41,15 @@ cd /opt
 git clone -b v5.6.4.2 https://github.com/aircrack-ng/rtl8812au.git
 cd rtl*
 
-if [[ $DISTID == "Raspbian" ]]; then
+if [[ $DISTID == "Debian" ]]; then
   sudo sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
   sudo sed -i 's/CONFIG_PLATFORM_ARM64_RPI = n/CONFIG_PLATFORM_ARM64_RPI = y/g' Makefile
 fi
 sudo make dkms_install
+
+sudo apt install python3-pip
+pip install gpsd-py3 google-api-python-client
+pip install --upgrade "protobuf<=3.20.1"
+cd /opt
+git clone https://github.com/ckoval7/kisStatic2Mobile.git
+cd kisStatic2Mobile
