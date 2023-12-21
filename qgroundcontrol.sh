6020b1d10d5e88265b434f8ba084330e627e9c6b
NullVibes <71892534+NullVibes@users.noreply.github.com>
2023-12-21 09:06:33 -0500

    Update qgroundcontrol.sh

--- qgroundcontrol.sh
+++ qgroundcontrol.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install speech-dispatcher libudev-dev libsdl2-dev patchelf build-essential curl qmake gcc -y
-cd /opt
-sudo git clone --recursive -j8 https://github.com/mavlink/qgroundcontrol.git
-sudo git submodule update --recursive
-cd qgroundcontrol
+sudo usermod -a -G dialout $USER
+sudo apt remove modemmanager -y
+sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
+sudo apt install libqt5gui5 -y
+sudo apt install libfuse2 -y
 
+cd /opt
+sudo wget https://d176tv9ibo4jno.cloudfront.net/latest/QGroundControl.AppImage
+
+#sudo apt install speech-dispatcher libudev-dev libsdl2-dev patchelf build-essential curl qmake gcc -y
+#sudo git clone --recursive -j8 https://github.com/mavlink/qgroundcontrol.git
+#sudo git submodule update --recursive
+
+sudo chmod +x QGroundControl.AppImage
+echo ""
+echo "To launch QGroundControl:  ./QGroundControl.AppImage"
