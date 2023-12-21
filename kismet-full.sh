fe946090f96d42c03f490cd9fb972ba7888baad9
NullVibes <71892534+NullVibes@users.noreply.github.com>
2023-12-21 13:51:30 -0500

    Update kismet-full.sh

--- kismet-full.sh
+++ kismet-full.sh
@@ -3,6 +3,9 @@
 DISTID=$(lsb_release -a 2> /dev/null | grep "Distributor ID" | awk '{print $3}')
 CODENAME=$(lsb_release -a 2> /dev/null | grep "Codename" | awk '{print $2}')
 RASPICONFPATH=$(which raspi-config)
+echo 'DISTID = '$DISTID
+echo 'CODENAME = '$CODENAME
+echo 'RASPICONFPATH = '$RASPICONFPATH
 echo ""
 echo "This installer will build the -NIGHTLY- (most up-to-date) package from Git." 
 read -p "Continue? (Y/n): " -n1 CONT
@@ -11,7 +14,7 @@ if [[ $CONT == "n" ]]; then
   exit
 fi
 
-wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg
+wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg &> /dev/null
 
 if [[ $DISTID == *"Ubuntu"* ]]; then
   echo 'Ubuntu detected...'
@@ -37,6 +40,7 @@ elif [[ $RASPICONFPATH == "/usr/bin/raspi-config" ]]; then
     echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/git/bookworm bookworm main' | sudo tee /etc/apt/sources.list.d/kismet.list &>/dev/null
   fi
   sudo apt install raspberrypi-kernel-headers -y
+  #linux-headers-6.1.21+
 fi
 
 sudo apt update
@@ -52,13 +56,13 @@ cd /opt
 sudo git clone -b v5.6.4.2 https://github.com/aircrack-ng/rtl8812au.git
 cd rtl*
 
-if [[ $DISTID == "Debian" ]] && [[ $RASPICONFPATH == "/usr/bin/raspi-config" ]]; then
+if [[ $RASPICONFPATH == "/usr/bin/raspi-config" ]]; then
   sudo sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
   sudo sed -i 's/CONFIG_PLATFORM_ARM64_RPI = n/CONFIG_PLATFORM_ARM64_RPI = y/g' Makefile
 fi
 sudo make dkms_install
 
-sudo apt install python3-pip -y 1> /dev/null
+sudo apt install python3-pip -y
 pip install gpsd-py3 google-api-python-client
 pip install --upgrade "protobuf<=3.20.1"
 cd /opt
