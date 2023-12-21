a57f371d120c112b608c029376ed5c1b9636d45c
NullVibes <71892534+NullVibes@users.noreply.github.com>
2023-12-21 12:37:06 -0500

    Update kis-src.sh

--- kis-src.sh
+++ kis-src.sh
@@ -3,9 +3,12 @@
 # Sleep is REQUIRED to give devices (GPS!)enough to time to be recognized by the OS
 sleep 30
 SITE='/etc/kismet/kismet_site.conf'
+REMOTE='/etc/kismet/kismet_remote.sh'
 
 # Remove interfaces already in monitor mode.
 cat /dev/null | tee $SITE &>/dev/null
+cat /dev/null | tee $REMOTE &>/dev/null
+echo '#!/bin/bash' | tee -a $REMOTE &>/dev/null
 
 echo 'server_name=Kismet' | tee -a $SITE &>/dev/null
 echo 'server_description=Mobile' | tee -a $SITE &>/dev/null
@@ -32,6 +35,7 @@ C=$(iw dev | grep -E -c -i 'phy#[0-9]')
 D=$(iw dev | grep -E -m 1 -i 'phy#[0-9]' | sed '$ s/[a-z#]//g')
 #echo 'mask_datasource_name=wlan0' | tee /etc/kismet/kismet_site.conf &>/dev/null
 
+#Probe for WiFi adapters
 if [ $C -ne 0 ]
 then
 	for (( j=0; j<=$D; j++ ))
@@ -43,9 +47,10 @@ then
 			F=$(iw dev | grep -E -m 1 -A 1 -i 'phy#'$j | awk '$1=="Interface"{print $2}')
 			G=$(echo $F | awk '$0=$NF' FS=)
 			#echo $F
-   			CHECKWIFI=$(sudo grep -c "source=$F:name=Wifi$G,channel_hop=true" $SITE)
+   			CHECKWIFI=$(sudo grep -c "source=$F:name=wlan$G,channel_hop=true" $SITE)
       			if [[ $CHECKWIFI -eq 0 || $CHECKWIFI == "" ]]; then
-				echo 'source='$F':name=Wifi'$G',channel_hop=true' | tee -a $SITE &>/dev/null
+				echo 'source='$F':name=wlan'$G',channel_hop=true' | tee -a $SITE &>/dev/null
+    				echo 'kismet_cap_linux_wifi --connect 127.0.0.1:3500 --tcp --fixed-gps 39.5,-75.5 --source=wlan'$F':name=Wifi'$G',type=linuxwifi,channel_hop=true &' &> $REMOTE
     			fi
 		fi
 	done
@@ -53,7 +58,7 @@ then
 fi
 
 
-#Probe for Ubertooth units
+#Probe for Ubertooth adapters
 J=$(lsusb | grep -E -c -i 'Ubertooth One')
 
 if [ $J -gt 0 ]
@@ -65,7 +70,7 @@ then
 fi
 
 
-#Probe for Bluetooth units
+#Probe for Bluetooth adapters
 K=$(hcitool dev | grep -E -c -i 'hci')
 
 if [ $K -gt 0 ]
@@ -73,11 +78,12 @@ then
  	for (( m=0; m<$K; m++ ))
  	do
   		echo 'source=hci'$m':name=linuxbt'$m',type=linuxbluetooth' | tee -a $SITE &>/dev/null
+    		echo 'kismet_cap_linux_bluetooth --connect 127.0.0.1:3500 --tcp --fixed-gps 39.5,-75.5 --source=hci'$m':name=hci'$m',type=linuxbt &' &> $REMOTE
   	done
 fi
 
 
-#Probe for RTL-SDR units
+#Probe for RTL-SDR adapters
 N=$(lsusb | grep -E -c -i 'RTL[[:alnum:]]{4,5}.DVB-T')
 Q="sudo grep -c channel="
 if [ $N -gt 0 ]
@@ -97,7 +103,7 @@ then
 fi
 
 
-# Probe for GPS units
+# Probe for GPS adapters
 H=$(ls -lh /dev | grep -E -c -i 'gps[0-9]')
 I=$(ls -lh /dev | grep -E -c -i 'ttyACM[0-9]')
 
