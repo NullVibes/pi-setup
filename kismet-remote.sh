54b00e0246b3cc3b0fbe817a30352965d488e774
NullVibes <71892534+NullVibes@users.noreply.github.com>
2023-12-21 09:13:49 -0500

    Update kismet-remote.sh

--- kismet-remote.sh
+++ kismet-remote.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
 
-# Install Kismet NIGHTLY via Kismet PPA, and RTL-SDR requirements
-wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key | sudo apt-key add -
-echo 'deb https://www.kismetwireless.net/repos/apt/git/bullseye bullseye main' | sudo tee /etc/apt/sources.list.d/kismet.list
-sudo apt update
-sudo apt install gpsd gpsd-clients rtl-sdr rtl-433 kismet vim -y
-
+kismet_cap_linux_wifi
+kismet_cap_linux_bluetooth
+kismet_cap_ubertooth_one
+kismet_cap_bladerf_wiphy
+kismet_cap_nrf_52840
