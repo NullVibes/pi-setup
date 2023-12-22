19791f07eb2a2e170e5667b0868ec8842d5edc8c
NullVibes <71892534+NullVibes@users.noreply.github.com>
2023-12-22 12:00:11 -0500

    Update qgroundcontrol.sh

--- qgroundcontrol.sh
+++ qgroundcontrol.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-
+#
+# QGroundControl pre-built binaries for linux are x86_64 images, and will not work on the RPi.
+# Everythng has to be built from source.
+#
 sudo apt update
 sudo usermod -a -G dialout $USER
 sudo apt remove modemmanager -y
-sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
-sudo apt install libqt5gui5 -y
-sudo apt install libfuse2 -y
+sudo apt install speech-dispatcher libudev-dev libsdl2-dev patchelf build-essential curl gcc qtcreator qmake openjdk-11-jdk-headless -y
+sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl libqt5gui5 libfuse2 -y
 
 cd /opt
-sudo wget https://d176tv9ibo4jno.cloudfront.net/latest/QGroundControl.AppImage
+sudo git clone --recursive -j8 https://github.com/mavlink/qgroundcontrol.git
+cd qgroundcontrol
+sudo git submodule update --recursive
+echo -e "DEFINES += DISABLE_AIRMAP\r\n" | tee user_config.pri
+mkdir build
+cd build
+qmake ../
+make -j3
 
-#sudo apt install speech-dispatcher libudev-dev libsdl2-dev patchelf build-essential curl qmake gcc -y
-#sudo git clone --recursive -j8 https://github.com/mavlink/qgroundcontrol.git
-#sudo git submodule update --recursive
-
-sudo chmod +x QGroundControl.AppImage
 echo ""
-echo "To launch QGroundControl:  ./QGroundControl.AppImage"
+echo "To launch QGroundControl:  ./staging/QGroundControl"
