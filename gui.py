d35c182248333b26d753fb37259355b49a08de68
NullVibes <71892534+NullVibes@users.noreply.github.com>
2024-01-15 14:00:50 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -42,11 +42,10 @@ def app_layout(self):
 def app1():
     uhfdir = "/opt/UHF-Sweep/"
     if os.path.exists(uhfdir + "uhf-sweep.sh") == True:
-        subprocess.run(["sudo " + uhfdir + " ./uhf-sweep.sh"], shell=True, capture_output=True, text=True)
+        subprocess.run(["sudo " + uhfdir + "./uhf-sweep.sh"], shell=True, capture_output=True, text=True)
         if os.path.exists(uhfdir + "uhf_sweep.csv") == True:
-            result = subprocess.run(["tail -n1 " + uhfdir + " uhf_sweep.csv | cut -d',' -f5"], shell=True, capture_output=True, text=True)
-            #lstBox1.insert(END, str(result.stdout))
-            lstBox1.insert(END, str(result))
+            result = subprocess.run(["tail -n1 " + uhfdir + "uhf_sweep.csv | cut -d',' -f5"], shell=True, capture_output=True, text=True)
+            lstBox1.insert(END, str(result.stdout))
             window.after(1000, app1)  # run again after 1000ms (1s)
         else:
             lstBox1.insert(END, str("Input File Not Found"))
