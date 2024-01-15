ec7f917421dbefe79d4b2f64579a06463be8c810
NullVibes <71892534+NullVibes@users.noreply.github.com>
2024-01-15 10:22:00 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -85,11 +85,11 @@ cApp2.place(x=0, y=0)
 cApp2.pack_forget()
 
 
-# --- Canvas: App #1 (EPC Hunter) ---
+# --- Canvas: App #1 (UHF Sweep) ---
 cApp1 = Canvas(window, height=400, width=800, bg="#22303C", bd='0', borderwidth=0, highlightthickness=0)
 cApp1.place(x=0, y=0)
 
-lstBox1 = Listbox(cApp1, height=3, width=10, bd='0')
+lstBox1 = Listbox(cApp1, height=3, width=20, bd='0')
 #lstBox1.pack(side = LEFT, fill = BOTH)
 scrollbar1 = Scrollbar(cApp1)
 #scrollbar1.pack(side = RIGHT, fill = BOTH)
