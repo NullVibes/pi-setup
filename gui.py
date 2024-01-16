233f7a51a5d92065504008210048f3d0afcaf295
NullVibes <71892534+NullVibes@users.noreply.github.com>
2024-01-16 11:32:05 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -45,10 +45,9 @@ def app1():
         subprocess.run(["sudo " + uhfdir + "./uhf-sweep.sh"], shell=True, capture_output=True, text=True)
         if os.path.exists(uhfdir + "uhf_sweep.csv") == True:
             LAST_TAG = subprocess.run(["tail -n1 " + uhfdir + "uhf_sweep.csv | cut -d',' -f5"], shell=True, capture_output=True, text=True)
-            print(str(LAST_TAG.stdout[:-1]))
             for i, lstBox1_item in enumerate(lstBox1.get(0, END)):
                 if lstBox1_item == str(LAST_TAG.stdout[:-1]):
-                    pass
+                    print(str(LAST_TAG.stdout[:-1]))
                 else:
                     lstBox1.insert(END, str(LAST_TAG.stdout[:-1]))
             window.after(1000, app1)  # run again after 1000ms (1s)
