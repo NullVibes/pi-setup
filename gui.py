74b7f0af4267e7c598db60fc11996d5f2e4ff21d
NullVibes <71892534+NullVibes@users.noreply.github.com>
2024-01-16 11:08:49 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -45,7 +45,7 @@ def app1():
         subprocess.run(["sudo " + uhfdir + "./uhf-sweep.sh"], shell=True, capture_output=True, text=True)
         if os.path.exists(uhfdir + "uhf_sweep.csv") == True:
             LAST_TAG = subprocess.run(["tail -n1 " + uhfdir + "uhf_sweep.csv | cut -d',' -f5"], shell=True, capture_output=True, text=True)
-            lstBox1.insert(END, str(LAST_TAG.stdout))
+            lstBox1.insert(END, str(LAST_TAG.stdout[:-1]))
             window.after(1000, app1)  # run again after 1000ms (1s)
         else:
             lstBox1.insert(END, str("Input File Not Found"))
