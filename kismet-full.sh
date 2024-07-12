55b25ed5599da52038a9ecd6e90cef9bfc0276bf
NullVibes <71892534+NullVibes@users.noreply.github.com>
2024-07-12 19:41:26 -0400

    Update kismet-full.sh

--- kismet-full.sh
+++ kismet-full.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Re-evaluate how we do this for DietPi...  Variables are empty...
 DISTID=$(lsb_release -a 2> /dev/null | grep "Distributor ID" | awk '{print $3}')
 CODENAME=$(lsb_release -a 2> /dev/null | grep "Codename" | awk '{print $2}')
 RASPICONFPATH=$(which raspi-config)
@@ -55,7 +56,7 @@ elif [[ $RASPICONFPATH == "/usr/bin/raspi-config" ]]; then
 fi
 
 sudo apt update
-sudo apt install build-essential vim dkms bluez gpsd gpsd-clients kismet tcpdump rtl-sdr rtl-433 -y
+sudo apt install build-essential vim dkms bluez gpsd gpsd-clients kismet tcpdump rtl-sdr rtl-433 chrony -y
 sudo usermod -aG kismet $USER
 sudo cp kis-src.sh /etc/kismet
 sudo chmod +x /etc/kismet/kis-src.sh
@@ -64,6 +65,17 @@ sudo cp kismet.service /etc/systemd/system
 sudo cp kisstatic2mobile.service /etc/systemd/system
 sudo systemctl daemon-reload
 #sudo systemctl enable kismet.service
+
+if [[ -e /etc/default/gpsd ]]; then
+  echo 'START_DAEMON="true' | sudo tee -a /etc/default/gpsd &>/dev/null
+  sudo sed -i 's/DEVICES=""/DEVICES="/dev/ttyUSB0"' /etc/default/gpsd
+  sudo sed -i 's/GPSD_OPTIONS=""/GPSD_OPTIONS="-n"' /etc/default/gpsd
+fi
+
+if [[ -e /etc/chrony/chrony.conf ]]; then
+  echo 'refclock SHM 0 refid GPS poll 2 precision 1e-3 offset 0.128' | sudo tee -a /etc/chrony/chrony.conf
+  sudo sed -i 's/pool 2.debian.pool.ntp.org iburst/#pool 2.debian.pool.ntp.org iburst' /etc/default/gpsd
+fi
 
 cd /opt
 sudo git clone -b v5.6.4.2 https://github.com/aircrack-ng/rtl8812au.git
@@ -81,6 +93,8 @@ pip install --upgrade "protobuf<=3.20.1"
 cd /opt
 sudo git clone https://github.com/ckoval7/kisStatic2Mobile.git
 
-echo ''
-echo 'kismet.service is currently READY, but DISABLED on boot.'
-echo 'To enable:  sudo systemctl enable kismet.service'
+echo 'Notes:'
+echo '- kismet.service is currently READY, but DISABLED on boot.'
+echo '- To enable:  sudo systemctl enable kismet.service'
+echo '- System clock will now use GPS as its time source.  Reboot is required.'
+
